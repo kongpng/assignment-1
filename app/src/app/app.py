b585c1ff4c7ef648f90903c212991fb4ad265a0a
sebastian dalbjørn-winblad <zhadowseb@hotmail.com>
2024-11-26 04:54:43 +0100

    Adding 1.2.3 and 1.2.4

--- app/src/app/app.py
+++ app/src/app/app.py
@@ -7,6 +7,58 @@ from typing import Optional
 
 class HelloWorld(toga.App):
 
+    def startup(self):
+        main_box = toga.Box(style=Pack(direction=COLUMN))
+
+        name_label = toga.Label(
+            text="Your name: ",
+            style=Pack(padding=(0, 5)),
+        )
+        self.name_input = toga.TextInput(style=Pack(flex=1))
+
+        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        name_box.add(name_label)
+        name_box.add(self.name_input)
+
+        button = toga.Button(
+            text="Say Hello!",
+            on_press=self.say_hello,
+            style=Pack(padding=5),
+        )
+
+        main_box.add(name_box)
+        main_box.add(button)
+
+        login_box = self.login_box_widget()
+        all_instances_box = self.all_instances_widget()
+        # TODO add Containers and Widgets to your all_instances_box
+        instance_box = self.instance_box()
+
+        logout_box = toga.Box(style=Pack(direction=COLUMN, flex=1))
+        logout_button = toga.Button(
+            text="Logout",
+            on_press=self.logout,  # Handler til logout-knappen
+            style=Pack(padding=10)
+        )
+        logout_box.add(logout_button)
+
+
+        option_container = toga.OptionContainer(
+            content=[
+                toga.OptionItem("Main box", main_box),
+                toga.OptionItem("Login", login_box),
+                toga.OptionItem("All instances", all_instances_box),
+                toga.OptionItem("Instance run", instance_box),
+                toga.OptionItem("Logout", logout_box),
+            ],
+            on_select=self.option_item_changed,
+            style=Pack(direction=COLUMN),
+        )
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = option_container
+        self.main_window.show()
+
     def login_box_widget(self) -> toga.Box:
         login_box = toga.Box(style=Pack(direction=COLUMN, flex=1))
         username_label = toga.Label(text="Username:", style=Pack(padding=(5)))
@@ -79,53 +131,85 @@ class HelloWorld(toga.App):
 
         return all_instances_box
 
-    def startup(self):
-        main_box = toga.Box(style=Pack(direction=COLUMN))
-
-        name_label = toga.Label(
-            text="Your name: ",
-            style=Pack(padding=(0, 5)),
-        )
-        self.name_input = toga.TextInput(style=Pack(flex=1))
-
-        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
-        name_box.add(name_label)
-        name_box.add(self.name_input)
-
-        button = toga.Button(
-            text="Say Hello!",
-            on_press=self.say_hello,
-            style=Pack(padding=5),
-        )
-
-        main_box.add(name_box)
-        main_box.add(button)
-
-        login_box = self.login_box_widget()
-        all_instances_box = self.all_instances_widget()
-        # TODO add Containers and Widgets to your all_instances_box
+    def instance_box(self) -> toga.Box:
         instance_box = toga.Box(style=Pack(direction=COLUMN, flex=1))
-        # TODO add Containers and Widgets to your instance_box
-        logout_box = toga.Box(style=Pack(direction=COLUMN, flex=1))
 
-        option_container = toga.OptionContainer(
-            content=[
-                toga.OptionItem("Main box", main_box),
-                toga.OptionItem("Login", login_box),
-                toga.OptionItem("All instances", all_instances_box),
-                toga.OptionItem("Instance run", instance_box),
-                toga.OptionItem("Logout", logout_box),
-            ],
-            on_select=self.option_item_changed,
-            style=Pack(direction=COLUMN),
+        role_items = list([' ', 'Doctor', 'Nurse', 'Patient'])
+        selected_role_item = 'Doctor'
+        events = [{'id':'Diagnose', 'label':'Diagnose', 'role':'Doctor'},
+                  {'id':'Operate', 'label':'Operate', 'role':'Doctor'},
+                  {'id':'Give treatment', 'label':'Give treatment', 'role':'Nurse'},
+                  {'id':'Take treatment', 'label':'Take treatment', 'role':'Patient'}]
+
+        label = toga.Label("Each instance will appear here")
+        instance_box.add(label)
+
+        ### Currentbox
+        current_instance_box = toga.Box(style=Pack(direction=ROW, flex=1))
+        
+        ### other box
+        other_instance_box = toga.Box(style=Pack(direction=COLUMN, flex=1))
+
+        current_role = toga.Label("Current role")
+        selected_role_label = toga.Label("Select other role")
+        self.role_selection = toga.Selection(
+            items=role_items,
+            value=role_items[0],
+            on_change=self.role_changed
         )
 
-        self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = option_container
-        self.main_window.show()
+        other_instance_box.add(current_role)
+        other_instance_box.add(selected_role_label)
+        other_instance_box.add(self.role_selection)
+        current_instance_box.add(other_instance_box)
+        ### End other box
+
+        ### Not added box
+        not_added_box = toga.Box(style=Pack(direction=COLUMN, flex=1))
+
+        current_instance_label = toga.Label("Current instance")
+        not_yet_added_label = toga.Label("Not added yet!")
+
+        not_added_box.add(current_instance_label)
+        not_added_box.add(not_yet_added_label)
+        current_instance_box.add(not_added_box)
+        ### End not added box
+
+        ### End Currentbox
+
+        ### Scrollcontainer
+        scroll_container = toga.ScrollContainer(style=Pack(flex=1))
+        event_box = toga.Box(style=Pack(direction=COLUMN, padding=10))
+
+        for event in events:
+            event_button = toga.Button(
+                text=f"{event['label']} (role: {event['role']})",
+                id=event['id'],
+                on_press=self.execute_event
+            )
+            event_box.add(event_button)
+
+        scroll_container.content = event_box
+        
+        ### End Scrollcontainer
+        
+        instance_box.add(current_instance_box)
+        instance_box.add(scroll_container)
+
+
+        return instance_box
 
     async def option_item_changed(self, widget):
         print("[i] You have selected another Option Item!")
+
+    async def role_changed(self, widget):
+        print(f'[i] You changed the role to {self.role_selection.value}!')
+
+    async def logout(self, widget):
+        print("You want to logout!")
+
+    async def execute_event(self, widget):
+        print(f'[i] You want to execute event: {widget.id}')
 
     async def say_hello(self, widget):
         async with httpx.AsyncClient() as client:
@@ -150,13 +234,11 @@ class HelloWorld(toga.App):
     async def delete_all_instances(self, widget):
         print("[i] Delete all instances!")
 
-
 def greeting(name):
     if name:
         return f"Hello, {name}"
     else:
         return "Hello, stranger"
-
 
 def main():
     return HelloWorld()
